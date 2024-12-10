a607c83bed6e961aea6030c426c45df42026822c
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-12-11 01:40:26 +0530

    updated user image in chat content element

--- frontend/src/ChatElement.jsx
+++ frontend/src/ChatElement.jsx
@@ -7,7 +7,9 @@ const ChatElement = function ({ data, role }) {
       <div className="chat-content">
         <div className="chat-details">
           <img
-            src={role === "assistant" ? activeAssistant.image : `/logo.png`}
+            src={
+              role === "assistant" ? activeAssistant.image : `/logo_dark.png`
+            }
             alt="user-img"
           />
           <div className="content">
